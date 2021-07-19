5b651858d513ddd1d74936e2f7a99afa54199103
Bobby Willmes <bobbywillmes@gmail.com>
2021-07-19 11:03:19 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.port || 3001
+const app = express()
+
+// Have Node serve the files for our built React app
+const buildPath = path.normalize(path.join(__dirname, '/client/build'))
+app.use(express.static(buildPath))
+
+// Handle GET requests to /api route
+app.get('/api', (req, res) => {
+  setTimeout(() => {
+    res.json({ message: 'Hello from server. '})
+  }, 1000)
+})
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(port, ()=> {
+  console.log(`Server running on port ${port}`)
+})
