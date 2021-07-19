ab0c820a17751034f471af0864deae66a238d419
Bobby Willmes <bobbywillmes@gmail.com>
2021-07-19 11:18:18 -0700

    Heroku Procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 const app = express()
 
 // Have Node serve the files for our built React app
